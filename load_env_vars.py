ba02f881d1d50004409fd27e4349bfdba4b5ba33
Simran <127680759+simran-261@users.noreply.github.com>
2024-12-20 21:06:43 +0530

    minor fixes

--- load_env_vars.py
+++ load_env_vars.py
@@ -1,16 +1,12 @@
 # load_env_vars.py
 
-# Import the required modules
 from dotenv import load_dotenv
 import os
 
-# Load the .env file
 load_dotenv()
 
 # Access the environment variables
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 
-# Now you can use these keys in your code
-print(OPENAI_API_KEY)  # For testing purposes (Make sure to avoid printing sensitive keys in production)
-print(GITHUB_TOKEN)
+
