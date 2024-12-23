2231a551c4451354f0c347051ff2c03da715f9b4
Simran <127680759+simran-261@users.noreply.github.com>
2024-12-23 23:59:21 +0530

    testing markdown translation

--- src/hooks/post_commit.py
+++ src/hooks/post_commit.py
@@ -1,7 +1,9 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+script_dir = os.path.dirname(__file__)
+src_dir = os.path.abspath(os.path.join(script_dir, '..', '..', 'src'))
+sys.path.insert(0, src_dir)
 from utils.translation import translate_text
 
 
