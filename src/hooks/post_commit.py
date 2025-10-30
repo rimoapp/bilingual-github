5990f6d02ae92879fa110340061ac5e0cf742e85
HrxSrv <harshitsrv2004@gmail.com>
2025-10-31 05:21:29 +0530

    Changed translation ignore file name

--- src/hooks/post_commit.py
+++ src/hooks/post_commit.py
@@ -14,7 +14,7 @@ sys.path.insert(0, src_dir)
 from utils.translation import translate_text, translate_incremental
 
 TARGET_LANGUAGES = ["en", "ja"]
-TRANSLATION_IGNORE_FILE = ".transignore"
+TRANSLATION_IGNORE_FILE = ".md_ignore"
 
 # Incremental translation thresholds
 DIFF_THRESHOLD_PERCENT = 50  # If diff > 30%, use full translation
